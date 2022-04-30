6249ae95c5a74a02d75be5127b200a50c1cbf227
Ivan Ivanenko <i.ivanenko@itransition.com>
2022-04-30 14:09:33 +0300

    init

---
+++ Api/Controllers/RequestsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestService.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RequestsController : ControllerBase
+{
+    
+}
