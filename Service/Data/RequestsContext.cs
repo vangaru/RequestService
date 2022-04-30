1d2413443b00144725f747cccf1f1480e70fc3f2
Ivan Ivanenko <i.ivanenko@itransition.com>
2022-05-01 00:23:00 +0300

    add DbContext and configure services

---
+++ Service/Data/RequestsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestService.Common;
+
+namespace RequestService.Data;
+
+public sealed class RequestsContext : DbContext
+{
+    public DbSet<Request>? Requests { get; set; }
+    public DbSet<Route>? Routes { get; set; }
+
+    public RequestsContext(DbContextOptions<RequestsContext> options): base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
--- Service/Program.cs
+++ Service/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using RequestService.Api.Services;
+using RequestService.Data;
+using RequestService.Services;
 using Service;
 
+const string defaultConnectionProp = "DefaultConnection";
+
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .ConfigureServices((hostContext, services) =>
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<RequestsContext>();
+        optionsBuilder.UseNpgsql(hostContext.Configuration.GetConnectionString(defaultConnectionProp));
+        services.AddScoped(_ => new RequestsContext(optionsBuilder.Options));
+        services.AddOptions();
+        services.AddTransient<IIntensityService, IntensityService>();
+        services.AddTransient<IIntervalService, IntervalService>();
+        services.AddHostedService<Worker>();
+    })
     .Build();
 
 await host.RunAsync();
