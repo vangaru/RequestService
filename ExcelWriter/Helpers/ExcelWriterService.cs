1bd525e483ee220322e56a7419c747a87f331e7b
Ivan Ivanenko <i.ivanenko@itransition.com>
2022-05-02 00:54:21 +0300

    ExcelWriterHelper: writing requests to db

--- ExcelWriter/Helpers/ExcelWriterService.cs
+++ ExcelWriter/Helpers/ExcelWriterService.cs
@@ -1,4 +1,6 @@
-﻿using RequestService.Common.Models;
+﻿using System.Data;
+using OfficeOpenXml;
+using RequestService.Common.Models;
 
 namespace RequestService.ExcelWriter.Helpers;
 
@@ -9,7 +11,33 @@ public static class ExcelWriterService
 {
     public static void Write(IEnumerable<Request> requests, string excelPath)
     {
-        
+        using var excelPackage = new ExcelPackage();
+        using var dataTable = new DataTable();
+        PopulateRequestColumns(dataTable);
+        PopulateRequestRows(dataTable, requests);
+    }
+
+    private static void PopulateRequestColumns(DataTable dataTable)
+    {
+        // Could have used reflection but im tired.
+        dataTable.Columns.Add("Id", typeof(string));
+        dataTable.Columns.Add("Origin", typeof(int));
+        dataTable.Columns.Add("Destination", typeof(int));
+        dataTable.Columns.Add("Seats count", typeof(int));
+        dataTable.Columns.Add("DateTime", typeof(string));
+    }
+
+    private static void PopulateRequestRows(DataTable dataTable, IEnumerable<Request> requests)
+    {
+        foreach (Request request in requests)
+        {
+            dataTable.Rows.Add(
+                request.Id,
+                request.Route?.Origin,
+                request.Route?.Destination,
+                request.SeatsCount,
+                request.RequestDateTime);
+        }
     }
 
     public static void Write(IEnumerable<RequestsPerHourSummary> summaries, string excelPath)
